a56989cdd76ae13ad4352772d48ffa0f0c0a54dd
Tres Seaver <tseaver@palladion.com>
2013-04-11 15:30:53 -0400

    In demo app, show using the CandidePropertysheet.

--- candide/resources.py
+++ candide/resources.py
@@ -1,15 +1,13 @@
 import colander
 import deform.widget
-
 from persistent import Persistent
-
 from substanced.content import content
 from substanced.property import PropertySheet
-from substanced.schema import (
-    Schema,
-    NameSchemaNode
-    )
+from substanced.schema import Schema
+from substanced.schema import NameSchemaNode
 from substanced.util import renamer
+
+from candide.property import CandidePropertysheet
 
 def context_is_a_document(context, request):
     return request.registry.content.istype(context, 'Document')
@@ -35,6 +33,7 @@ class DocumentPropertySheet(PropertySheet):
     add_view='add_document', 
     propertysheets = (
         ('Basic', DocumentPropertySheet),
+        ( 'Custom', CandidePropertysheet),
         ),
     )
 class Document(Persistent):
@@ -44,5 +43,3 @@ class Document(Persistent):
     def __init__(self, title='', body=''):
         self.title = title
         self.body = body
-
-        
